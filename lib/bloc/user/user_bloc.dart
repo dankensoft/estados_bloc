4d136019f502a181af12444dca2989bd4ee73910
Danniels Castillo <danniels.magana@gmail.com>
2022-09-26 21:57:12 -0600

    First Part User BLoC

---
+++ lib/bloc/user/user_bloc.dart
@@ -0,0 +1,5 @@
+import 'package:meta/meta.dart';
+import 'package:estados/models/user.dart';
+
+part 'user_event.dart';
+part 'user_state.dart';
---
+++ lib/bloc/user/user_state.dart
@@ -0,0 +1,9 @@
+part of 'user_bloc.dart';
+
+@immutable
+abstract class UserState {
+  final bool existUser;
+  final User? user;
+
+  const UserState({this.existUser = false, this.user});
+}
